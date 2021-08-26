11bdd6e6b4415b4c8d197016633b4f6b5d37c6da
Paul-Semenko <78956330+Paul-Semenko@users.noreply.github.com>
2021-08-26 17:39:41 +0300

    commit-12-02

--- src/js/notifications.js
+++
@@ -1,8 +0,0 @@
-import { alert, info, success, error } from'../node_modules/@pnotify/core/dist/PNotify.js';
-import * as PNotifyMobile from'../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
-
-
-
-
-
-export default error;
